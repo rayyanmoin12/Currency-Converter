1d29ec2663e69e7483143ff067cf3f3d89b6a736
rayyanmoin12 <166857521+rayyanmoin12@users.noreply.github.com>
2024-07-15 21:48:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let currency = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.52,
+    PKR: 277.6
+};
+let ans = await inquirer.prompt([{
+        type: "list",
+        name: "from",
+        message: "Which currency would you like to convert from?",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        type: "list",
+        name: "to",
+        message: "Which currency would you like to convert to?",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your amount:"
+    }
+]);
+console.log(currency[ans.to] / currency[ans.from] * ans.amount);
